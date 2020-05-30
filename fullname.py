dfd2403f8c26f923d421faef79cc433741e55e60
Enes Secer <enesecer@gmail.com>
2020-05-30 13:52:27 -0400

    Test first commit

---
+++ fullname.py
@@ -0,0 +1,11 @@
+first_name = 'John'
+last_name = 'Rambo'
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+print("Hi again! Clarusway!")
+print("Testing to check commiting specific lines")
+
+
